58530945cfbb026f26cda56e779fa2785b9d07e2
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-09-09 00:00:17 -0300

    Componentes Footer e Header adicionados

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header class="header">
+            <div class="logo">
+                <img src="imagens/logo-no-background.svg" alt="Logo Phisecare" />
+            </div>
+            <nav class="nav">
+                <a href="#" class="nav-link">Baixar App</a>
+                <a href="#" class="nav-link">Cadastre-se</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
