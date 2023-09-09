deb6b41492ad1e5b3d7bd7959afb5170fa00d9f8
Vitor Ipiranga <vitorireboucas@edu.unifor.br>
2023-09-08 23:59:09 -0300

    Rotas adicionadas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import UserComponent from "./Components/Users/Users";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Layout from "./Pages/Layout/Layout";
+import Cadastro from "./Pages/Cadastro/Cadastro";
+import Page404 from "./Pages/Page404/Page404";
+import Login from "./Pages/Login/Login";
 
 function App() {
 
@@ -9,9 +14,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/Home"></Route>
-          <Route></Route>
-          <Route></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Page404 />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
